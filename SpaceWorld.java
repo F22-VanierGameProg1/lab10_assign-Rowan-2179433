9bf16243d5badd05be8adda32376c686c81c536f
RowanLajoie <2179433@edu.vaniercollege.qc.ca>
2022-11-18 18:24:03 -0500

    I added code to transition from SpaceWorld to EndScreenWorld and vice versa

--- SpaceWorld.java
+++ SpaceWorld.java
@@ -26,6 +26,10 @@ public class SpaceWorld extends SimulationWorld
             SpaceShip player = list.get(0);
             cameraCenter.setY(player.getPosition().getY() + 6.0);
         }
+        else 
+        {
+            transitionToWorld(new EndScreenWorld(getObjects(Score.class).get(0).getScore())); 
+        }
 
         
     }
